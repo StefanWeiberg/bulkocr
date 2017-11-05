cdeec50b6db4ac69c6265839446098dc13610554
Stefan Weiberg <StefanWeiberg@users.noreply.github.com>
2017-11-05 15:57:13 +0100

    use subfolder for united pdf
    this allows the creation of several merged subversions instead of merging the previously merged pdf into the next run

--- ocrpdf.sh
+++ ocrpdf.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd $1
 parallel "tesseract {} {} -l deu+eng pdf" ::: *.jpg
-pdfunite $(ls -v *.pdf) $(date "+%Y%m%d-%H%M%S").pdf
+mkdir -p OCR
+pdfunite $(ls -v *.pdf) OCR/$(date "+%Y%m%d-%H%M%S").pdf
